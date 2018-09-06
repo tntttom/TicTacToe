2380de7e2bb78d887d953ecf4c3cadc48622bfa3
tommynguyen <tnguyen21@luc.edu>
2018-09-06 13:45:57 -0500

    Added ability to show which tile the player chose Added a switch statement to fulfil this functionality Need to: - Two player functionality - Check if users won - Refactor to new classes?

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,17 @@ import java.util.Scanner;
 public class Main {
 
     //Variables
-    Scanner scan = new Scanner(System.in);
+    static Scanner scan = new Scanner(System.in);
     public static String[][] grid = new String[3][3];
+    public static String spot;
+    public static boolean gameOn = true;
 
 
     //SEPERATE AS MUCH CODE FROM MAIN TO KEEP IT CLEAN
     public static void main(String args[]){
 
         buildLayout();
-        beginGame();
+        welcomeGame();
     }
 
 
@@ -39,10 +41,64 @@ public class Main {
     }
 
 
-    private static void beginGame() {
+    private static void welcomeGame() {
         System.out.println();
         System.out.println("Welcome to Tic Tac Toe!");
         System.out.println("Player one will be X and player two will b O");
 
+        do{
+            beginGame();
+        } while (gameOn);
+
+    }
+
+    private static void beginGame() {
+        //2 Player or Computer?
+        System.out.println("Player one choose a spot (top left,middle middle)");
+        String input = scan.nextLine();
+
+        selectSpot(input, "one");
+
+        System.out.println(spot);
+        separateGrid();
+
+        //Check if someone won
+
+    }
+
+    private static void selectSpot(String in, String player) {
+
+        switch(in) {
+            default:
+                return;
+            case "top left":
+                spot = grid[0][0];
+                grid[0][0] = "o";
+                break;
+            case "top middle":
+                spot = grid[0][1];
+                break;
+            case "top right":
+                spot = grid[0][2];
+                break;
+            case "middle left":
+                spot = grid[1][0];
+                break;
+            case "middle middle":
+                spot = grid[1][1];
+                break;
+            case "middle right":
+                spot = grid[1][2];
+                break;
+            case "bottom left":
+                spot = grid[2][0];
+                break;
+            case "bottom middle":
+                spot = grid[2][1];
+                break;
+            case "bottom right":
+                spot = grid[2][2];
+                break;
+        }
     }
 }
